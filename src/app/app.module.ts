758a2c1b21d7c83b8f3253bf1b77f416e2ada883
sportyCodey <drewhcoder15@gmail.com>
2018-08-21 14:33:28 -0500

    Updated to Angular 6. Added scss files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent
   ],
   imports: [
     BrowserModule
